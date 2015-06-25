6e01a18c6f0e3b444d259bc35237479fdbbfa653
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-06-25 11:58:37 +0200

    add compatibility with vm-docker

--- vagrant/always.sh
+++ vagrant/always.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /vagrant
+sudo docker-compose up -d
 sudo docker-compose start
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,4 +3,3 @@ apt-get install -y python-pip
 cd /vagrant
 pip install -U docker-compose
 sudo docker-compose build
-sudo docker-compose up -d
