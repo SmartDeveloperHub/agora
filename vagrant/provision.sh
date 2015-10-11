0743457cfb041902192a6f9fc84aecb9df591f44
fserena <fernando.serena@centeropenmiddleware.com>
2015-10-11 23:32:14 +0200

    Install examples on provisioning

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
  
+# Clone examples
+apt-get install python-dev python-virtualenv -y
+apt-get remove python-pip
+easy_install pip
+git clone https://github.com/smartdeveloperhub/agora-examples.git
+cd agora-examples/ld-movies-service
+pip install -r requirements.txt
+
+# Build docker containers
 cd /vagrant
 sudo docker-compose build
+
